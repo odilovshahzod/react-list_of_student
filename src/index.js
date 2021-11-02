689268a932011106081900a058e33636b17d13c2
odilovshahzod <84099735+odilovshahzod@users.noreply.github.com>
2021-11-02 18:19:00 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import Title from "./fayl2/Title"
+
+ReactDOM.render(
+  <Title />,
+  document.getElementById('root')
+);
+reportWebVitals();
